5366e9d4d94a2c28fd0a1688d3fc6c5c3c7864f8
Dell <Dell@ZISHAN>
2021-09-14 13:10:09 +0530

    Add project files.

---
+++ UnaryOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UnaryOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7;
+            int b;
+            Console.WriteLine("The original value is" + a);
+            a++;
+            Console.WriteLine("The First increamented value is" + a++);
+            b = a++;
+            Console.WriteLine("a:{0},b:{1}" ,a ,b);
+            b += a;
+            Console.WriteLine("a:{0},b:{1}", a, b);
+
+
+        }
+    }
+}
